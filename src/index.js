c82010d4899a186ce8f90ef573ee8af740b4ae05
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-07-13 13:35:36 +0900

    mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -161,3 +161,30 @@ document.getElementById("app").innerHTML = `
 // console.log(arr8);
 // arr8[0] = 100
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+// const nameArr = ["中田", "山田", "こうせい"];
+//  for (let index = 0; index < nameArr.length; index++){
+//    console.log(`${index + 1}番目は，${nameArr[index]}です。`);
+//  }
+
+// const nameArr2 = nameArr.map((name) => name)
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は，${name}です。`))
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => num % 2 === 0)
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "こうせい") {
+//     return name
+//   }　 else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
